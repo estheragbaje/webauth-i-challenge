c574582296d5328e48f8950af7fa5e0a7e597542
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-12 14:18:38 +0100

    Created the sessionConfig object

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const session = require("express-session")
+const session = require("express-session");
 
 const server = express();
 const userRouter = require("./users/user-router");
 
+const sessionConfig = {
+  name: "apple",
+  secret: "make this long and keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api", userRouter);
 
